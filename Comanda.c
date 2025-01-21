36e8b9c393d04e1a510a844271385e467066f99f
gaalpos <gaalpos@proton.me>
2025-01-21 13:48:29 +0100

    01/21/2025 13:48:29

---
+++ Comanda.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+Ejercicio donde pides un numero de platos,
+cada plato tendra su precio y nombre, que mete el usuario.
+luego hace un resumen del pedido, sumando el precio de los platos.
+
+*/
+
+void menu(){
+
+printf("Numero de platos disponibles en el menu");
+printf("")
+
+
+
+
+}
+
+main (){
+
+
+menu();
+}
